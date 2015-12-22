c1356e35b002a6bcad56ea8e0cc061dbba8f1d36
HAI_Support Laurila Tommi <tommi.laurila@staff.jedu.fi>
2015-12-22 14:19:46 +0200

    improve graphics and sound

--- eka_pythoni.py
+++ eka_pythoni.py
@@ -11,6 +11,7 @@ black = (0,0,0)
 red = (255,0,0)
 blue = (0,0,255)
 green = (0,255,0)
+yellow = (255,255,0)
 skyblue = (40,195,255)
 
 display_width = 800
@@ -20,16 +21,15 @@ block_size = 20
 
 apple_size = 30
 apple_y_pos = 0
-apple_fall_speed = 5
 
 apple_value_plain = 10
 apple_value_bonus = 50
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('Matopeli')
+pygame.display.set_caption('FlakeSnake')
 
-# graphics from openclipart.org
-game_icon = pygame.image.load('snowflake.png')
+# graphics selfmade or taken from openclipart.org
+game_icon = pygame.image.load('snowflake_white.png')
 pygame.display.set_icon(game_icon)
 
 img_snakehead = pygame.image.load('snakehead.png')
@@ -37,6 +37,8 @@ img_snakebody = pygame.image.load('snakebody.png')
 
 img_snowflake_white = pygame.image.load('snowflake_white.png')
 img_snowflake_golden = pygame.image.load('snowflake_golden.png')
+
+img_background = pygame.image.load('background.png')
 
 # sound effect made on bfxr.net
 snd_pickup = pygame.mixer.Sound('pickup.wav')
@@ -51,36 +53,6 @@ font_large = pygame.font.SysFont(None, 44)
 
 # () = tuple
 # [] = list
-
-def game_intro():
-    intro = True
-    
-    while intro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_c:
-                    intro = False
-                if event.key == pygame.K_q:
-                    pygame.quit()
-                    quit()
-            
-        gameDisplay.fill(white)
-        message_to_screen("Tervetuloa Matopeliin",
-                          green,
-                          -50,
-                          "large")
-        message_to_screen("Syo omenoita",
-                          red,
-                          -20,
-                          "medium")
-        message_to_screen("Press C to play, P to pause, Q to quit.",
-                          red,
-                          10,
-                          "medium")
-        pygame.display.update()
 
 
 def pause():
@@ -110,8 +82,8 @@ def pause():
 
 
 def score(score):
-    text = font_small.render("Score: " + str(score), True, black)
-    gameDisplay.blit(text, [0,0])
+    text = font_small.render("Pisteet: " + str(score), True, white)
+    gameDisplay.blit(text, [4,4])
 
 
 def rand_apple_gen():
@@ -124,9 +96,9 @@ def rand_apple_gen():
 def generate_apple():
     apple_value = apple_value_plain
     
-    # generate new apple position
+    # generate new apple random position (at top half of screen)
     rand_apple_x = round(random.randrange(0, display_width-apple_size))
-    rand_apple_y = round(random.randrange(0, display_height-apple_size))
+    rand_apple_y = round(random.randrange(0, display_height/2-apple_size))
 
     # is the new apple a bonus apple?
     apple_type = round(random.randrange(1, 10))
@@ -172,16 +144,94 @@ def message_to_screen(msg, color, y_displace=0, size="small"):
     gameDisplay.blit(text_surf, text_rect)
 
 
+def game_intro():
+    # intro music by http://freesound.org/people/djgriffin/sounds/251289/
+    pygame.mixer.music.load('djgriffin.wav')
+    pygame.mixer.music.play(-1)
+    
+    intro = True
+    apple_fall_speed = 2
+
+    # draw falling flakes
+    rand_apple_x, rand_apple_y, apple_value = generate_apple()
+    apple_y_pos = rand_apple_y + apple_fall_speed
+    
+    flake_dir = "right"
+    flake_original_x = rand_apple_x
+    flake_max_x_displacement = 30
+    
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    intro = False
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
+                    
+        gameDisplay.blit(img_background, (0,0))
+
+        # draw apple zig-zag falling
+        if flake_dir == "right" and rand_apple_x < flake_original_x + flake_max_x_displacement:
+            rand_apple_x = rand_apple_x + apple_fall_speed
+        elif flake_dir == "right" and rand_apple_x >= flake_original_x + flake_max_x_displacement:
+            flake_dir = "left"
+        elif flake_dir == "left" and rand_apple_x > flake_original_x - flake_max_x_displacement:
+            rand_apple_x = rand_apple_x - apple_fall_speed
+        elif flake_dir == "left" and rand_apple_x <= flake_original_x - flake_max_x_displacement:
+            flake_dir = "right"
+
+        # draw apple
+        gameDisplay.blit(img_snowflake_white, (rand_apple_x, apple_y_pos))            
+        apple_y_pos += apple_fall_speed
+        
+        # if apple falls out of screen, generate new apple
+        if apple_y_pos > display_height:  
+            rand_apple_x, apple_y_pos, apple_value = generate_apple()
+            flake_original_x = rand_apple_x
+        
+        message_to_screen("FlakeSnake",
+                          green,
+                          -100,
+                          "large")
+        message_to_screen("Sieppaa lumihiutaleita ennen kun ne putoavat maahan ja",
+                          white,
+                          -20,
+                          "small")
+        message_to_screen("kasvattavat lumihankea. Varo lumihankea ja ruudun reunoja.",
+                          white,
+                          10,
+                          "small")
+        message_to_screen("C = uusi peli, P = tauko, Q = lopetus",
+                          yellow,
+                          50,
+                          "small")
+        pygame.display.update()
+        clock.tick(20)
+
+    pygame.mixer.music.stop()
+
+
 # =============== MAIN GAME LOOP ==================
 def gameLoop():
     # music by: https://www.freesound.org/people/joshuaempyre/sounds/251461/
     pygame.mixer.music.load('joshuaempyre.wav')
     pygame.mixer.music.play(-1)
     
-    tick_rate = 10
+    tick_rate = 20
+
+    snow_grow_height = 10
+    bottom_line = display_height
     
     global snake_dir
     snake_dir = "right"
+    snake_move_speed = 6
+
+    apple_fall_speed = 3
+    missed_apples = 0
     
     gameExit = False
     gameOver = False
@@ -191,7 +241,7 @@ def gameLoop():
     lead_x = display_width/2
     lead_y = display_height/2
     # starting direction for movement
-    lead_x_change = tick_rate
+    lead_x_change = snake_move_speed
     lead_y_change = 0
 
     snakelist = []
@@ -200,6 +250,10 @@ def gameLoop():
     # rand_apple_x, rand_apple_y = rand_apple_gen()
     rand_apple_x, rand_apple_y, apple_value = generate_apple()
     apple_y_pos = rand_apple_y + apple_fall_speed
+
+    flake_dir = "right"
+    flake_original_x = rand_apple_x
+    flake_max_x_displacement = 30
     
     while not gameExit:
         while gameOver == True:
@@ -232,33 +286,44 @@ def gameLoop():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     snake_dir = "left"
-                    lead_x_change = -block_size
+                    lead_x_change = -snake_move_speed
                     lead_y_change = 0
                 elif event.key == pygame.K_RIGHT:
                     snake_dir = "right"
-                    lead_x_change = block_size
+                    lead_x_change = snake_move_speed
                     lead_y_change = 0
                 elif event.key == pygame.K_UP:
                     snake_dir = "up"
-                    lead_y_change = -block_size
+                    lead_y_change = -snake_move_speed
                     lead_x_change = 0
                 elif event.key == pygame.K_DOWN:
                     snake_dir = "down"
-                    lead_y_change = block_size
+                    lead_y_change = snake_move_speed
                     lead_x_change = 0
                 elif event.key == pygame.K_p:
                     pause()
 
-        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+        # snake crosses boundaries
+        if lead_x >= display_width or lead_x < 0 or lead_y >= bottom_line or lead_y < 0:
             gameOver = True
 
         lead_x += lead_x_change
         lead_y += lead_y_change
 
-        # fill background
-        gameDisplay.fill(skyblue)
+        # draw apple zig-zag falling
+        if flake_dir == "right" and rand_apple_x < flake_original_x + flake_max_x_displacement:
+            rand_apple_x = rand_apple_x + apple_fall_speed
+        elif flake_dir == "right" and rand_apple_x >= flake_original_x + flake_max_x_displacement:
+            flake_dir = "left"
+        elif flake_dir == "left" and rand_apple_x > flake_original_x - flake_max_x_displacement:
+            rand_apple_x = rand_apple_x - apple_fall_speed
+        elif flake_dir == "left" and rand_apple_x <= flake_original_x - flake_max_x_displacement:
+            flake_dir = "right"
 
-        # draw apple
+        # draw background
+        gameDisplay.blit(img_background, (0,0))
+
+        # change apple image according to value
         if apple_value == apple_value_plain:
             gameDisplay.blit(img_snowflake_white, (rand_apple_x, apple_y_pos))
         elif apple_value == apple_value_bonus:
@@ -267,10 +332,20 @@ def gameLoop():
         apple_y_pos += apple_fall_speed
         
         # if apple falls out of screen
-        if apple_y_pos > display_height:
+        if apple_y_pos > bottom_line:
+            missed_apples += 1
+
+            # every fifth apple makes the bottom line snow grow upwards
+            if missed_apples % 5 == 0:
+                bottom_line -= snow_grow_height
+                
             # generate new apple
             rand_apple_x, apple_y_pos, apple_value = generate_apple()
+            flake_original_x = rand_apple_x
 
+        # draw bottom line snow
+        pygame.draw.rect(gameDisplay, white, [0, bottom_line, display_width, display_height-bottom_line])
+        
         # the snake
         snake_head = []
         snake_head.append(lead_x)
@@ -293,16 +368,18 @@ def gameLoop():
         pygame.display.update()
 
         # eat apple = move it to new position
-        if lead_x > rand_apple_x and lead_x < rand_apple_x + apple_size or lead_x + block_size > rand_apple_x and lead_x + block_size < rand_apple_x + apple_size:
+        if lead_x > rand_apple_x and lead_x < rand_apple_x + apple_size or lead_x + apple_size > rand_apple_x and lead_x + block_size < rand_apple_x + apple_size:
             if lead_y > apple_y_pos and lead_y < apple_y_pos + apple_size:
                 snd_pickup.play()
                 rand_apple_x, rand_apple_y, apple_value = generate_apple()
+                flake_original_x = rand_apple_x
                 score_points += apple_value
                 snake_length += 1
                 tick_rate += 1
             elif lead_y + block_size > apple_y_pos and lead_y + block_size < apple_y_pos + apple_size:
                 snd_pickup.play()
                 rand_apple_x, rand_apple_y, apple_value = generate_apple()
+                flake_original_x = rand_apple_x
                 score_points += apple_value
                 snake_length += 1
                 tick_rate += 1
